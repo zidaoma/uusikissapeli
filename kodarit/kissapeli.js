2297138ca955c4a6c54d58b2fe0be9d77655369b
MRNOEL223 <nporhan11@gmail.com>
2020-09-17 19:34:29 +0300

    kissa

--- kodarit/kissapeli.js
+++ kodarit/kissapeli.js
@@ -1,5 +1,10 @@
 let taustakuva;
 let kissakuva;
+let kissa;
+
+let lautan_leveys = 80;
+let lautanY = 350;
+
 let taustan_leveys =800;
 let taustan_korkeus = 400;
 
@@ -9,12 +14,43 @@ function preload() {
 }
 function setup() {
   var canvas = createCanvas(taustan_leveys, taustan_korkeus);
+
+  kissa = new Kissa();
 }
 
 function draw() {
   image(taustakuva, 0, 0, taustan_leveys, taustan_korkeus);
+
+  luo_lautta();
+  kissa.liikuta();
 }
 
 function luo_lautta() {
+  fill('red');
+     rect(mouseX, taustan_korkeus - 50, lautan_leveys, 30, 30, 20, 0, 0 )
+}
 
+class Kissa {
+constructor() {
+  this.X = 30;
+  this.Y = 200;
+  this.leveys = 50;
+  this.korkeus= 50;
+  this.Xnopeus = 2;
+  this.Ynopeus = -2;
+  }
+  liikuta() {
+    image(kissakuva, this.X, this.Y, this.leveys, this.korkeus)
+
+    this.X += this.Xnopeus;
+    this.Ynopeus += 0.05;
+
+if (this.Y += this.korkeus/ 2 > lautanY) {
+  if(this.X > mouseX && this.X < mouseX + lautan_leveys) {
+    this.Ynopeus = -abs(this.Ynopeus);
+  }
+}
+
+    this.Y += this.Ynopeus;
+  }
 }
